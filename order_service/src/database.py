ad20fd1fb1183a2753dcc3ba143c00e69abbe23c
Uladzislau <uladzislau.savitskiy@outlook.com>
2024-07-17 23:20:44 +0300

    update database

--- order_service/src/database.py
+++ order_service/src/database.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://user:password@db/order_db"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@db/order_db")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
